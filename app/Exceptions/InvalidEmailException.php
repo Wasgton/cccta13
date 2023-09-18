9ea0c25c2503c2eb150da23c76244739e68bdc26
junior <wasgton.it@gmail.com>
2023-09-18 10:36:36 -0300

    feat(AccountService): SignUp
    Implementing custom Exceptions;

---
+++ app/Exceptions/InvalidEmailException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Throwable;
+
+class InvalidEmailException extends \Exception
+{
+    protected $message = 'Invalid Email!';
+
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
---
+++ app/Exceptions/InvalidNameException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Throwable;
+
+class InvalidNameException extends \Exception
+{
+    protected $message = 'Invalid Name!';
+
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
