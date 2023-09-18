1314281cb3c3ae76be40fa68d284270ce25786cc
junior <wasgton.it@gmail.com>
2023-09-18 10:35:58 -0300

    feat(AccountService): SignUp
    Implementing SignUp method to pass the tests

--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -2,21 +2,57 @@
 
 namespace App\Services;
 
-use Config\Connection;
+use App\CPFValidator;
+use App\Exceptions\EmailAlreadyRegisteredException;
+use App\Exceptions\InvalidCPFException;
+use App\Exceptions\InvalidEmailException;
+use App\Exceptions\InvalidNameException;
+use config\Connection;
+use Ramsey\Uuid\Uuid;
 
 class AccountService
 {
+    /**
+     * @throws EmailAlreadyRegisteredException
+     */
     public function signUp(array $input)
     {
-        $input['account_id'] = \Ramsey\Uuid\Uuid::uuid4()->toString();
-        $input['verification_code'] = \Ramsey\Uuid\Uuid::uuid4()->toString();
+        if (!preg_match('/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i', $input['email'])) {
+            throw new InvalidEmailException();
+        }
+        $cpfValidator = new CPFValidator($input['cpf']);
+        if (!$cpfValidator->validate()) {
+            throw new InvalidCPFException();
+        }
+        if (!preg_match('/^[a-zA-Z] [a-zA-Z]/i', $input['name'])) {
+            throw new InvalidNameException();
+        }
+
+        $input['account_id'] = Uuid::uuid4()->toString();
+        $input['verification_code'] = Uuid::uuid4()->toString();
         $connection = new Connection();
-        $stmt = $connection->query(
-            "INSERT INTO accounts (account_id, name, email, cpf, isPassenger, verification_code) 
+
+        $account = $connection->query(
+            "SELECT * FROM account WHERE email = :email",
+            ['email' => $input['email']]
+        );
+        if($account->rowCount() > 0) {
+            throw new EmailAlreadyRegisteredException();
+        }
+        $connection->query(
+            "INSERT INTO account (account_id, name, email, cpf, is_passenger, verification_code)
                  VALUES (:account_id, :name, :email, :cpf, :isPassenger, :verification_code)",
             $input
         );
+        
+        
+        
         return $input;
+    }
+
+    private function sendEmail()
+    {
+        return true;
     }
 
 }
